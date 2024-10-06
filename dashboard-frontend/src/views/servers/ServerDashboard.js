b59687a6e27ef183cce160373546472b91e2c7d2
plavy <tinplavec@gmail.com>
2024-10-06 10:57:36 +0200

    get data from ssh server

--- dashboard-frontend/src/views/servers/ServerDashboard.js
+++ dashboard-frontend/src/views/servers/ServerDashboard.js
@@ -1,11 +1,13 @@
 import React, { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
 import classNames from 'classnames'
 
 const ServerDashboard = () => {
 
+  const { id } = useParams();
   const [message, setMessage] = useState("");
   useEffect(() => {
-    fetch("http://localhost:8000/servers")
+    fetch("http://localhost:8000/servers/" + id)
       .then((res) => res.json())
       .then((data) => setMessage(data));
   }, []);
