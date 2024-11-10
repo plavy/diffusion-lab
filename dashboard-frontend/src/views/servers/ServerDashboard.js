a2e9a741bbb83ff33a8f0e30f90da6c511fad6ea
plavy <tinplavec@gmail.com>
2024-11-10 21:53:33 +0100

    add server status notifs

--- dashboard-frontend/src/views/servers/ServerDashboard.js
+++ dashboard-frontend/src/views/servers/ServerDashboard.js
@@ -3,11 +3,14 @@ import { useParams } from "react-router-dom";
 import classNames from 'classnames';
 import axios from "axios";
 import { getAuthHeader } from "../../utils";
+import { CAlert, CButton, CSpinner } from "@coreui/react";
+import CIcon from "@coreui/icons-react";
+import { cilCheck, cilWarning } from "@coreui/icons";
 
 const ServerDashboard = () => {
 
   const { id } = useParams();
-  
+
   const [metadata, setMetadata] = useState("");
   useEffect(() => {
     axios.get("http://localhost:8000/servers/" + id, {
@@ -20,10 +23,29 @@ const ServerDashboard = () => {
   return (
     <>
       <h1>{metadata.name}</h1>
+
+      <CAlert id="statusChecking" color="info">
+        <CSpinner size="sm"/> Checking server status...
+      </CAlert>
+      <CAlert id="statusSuccess" color="success">
+        <CIcon icon={cilCheck}/> Server is reachable
+      </CAlert>
+      <CAlert id="statusUnreachable" color="danger">
+        <CIcon icon={cilWarning}/> Server is unreachable
+      </CAlert>
+      <CAlert id="statusAuthFail" color="danger">
+        <CIcon icon={cilWarning}/> Authentication is not working
+      </CAlert>
+
       <div>ID: {metadata.id}</div>
       <div>Hostname: {metadata.hostname}</div>
       <div>Port: {metadata.port}</div>
       <div>Username: {metadata.username}</div>
+
+      <CButton type="submit" color="primary" className="mt-2">
+        Prepare or update environment
+      </CButton>
+
     </>
   )
 }
