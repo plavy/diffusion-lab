78e23e035f2402a812a0f9e8654c0efc5550691d
plavy <tinplavec@gmail.com>
2024-11-10 19:15:09 +0100

    add train modal

--- dashboard-frontend/src/views/datasets/DatasetDashboard.js
+++ dashboard-frontend/src/views/datasets/DatasetDashboard.js
@@ -13,6 +13,15 @@ import {
   CAccordionItem,
   CAccordionHeader,
   CAccordionBody,
+  CModal,
+  CModalTitle,
+  CModalHeader,
+  CModalBody,
+  CModalFooter,
+  CFormSelect,
+  CForm,
+  CFormInput,
+  CBadge
 } from "@coreui/react";
 
 
@@ -29,6 +38,9 @@ const DatasetDashboard = () => {
     }).then((res) => setMetadata(res.data));
   }, [id]);
 
+  const [trainVisible, setTrainVisible] = useState(false);
+
+
   return (
     <div className="d-flex flex-column h-100">
       <div className="row flex-grow-1">
@@ -41,15 +53,15 @@ const DatasetDashboard = () => {
 
           <CCardGroup>
             <CCard>
-              <CCardImage src="src/assets/images/angular.jpg" />
+              <CCardImage src="/src/assets/images/angular.jpg" />
             </CCard>
 
             <CCard>
-              <CCardImage src="src/assets/images/react.jpg" />
+              <CCardImage src="/src/assets/images/react.jpg" />
             </CCard>
 
             <CCard>
-              <CCardImage src="src/assets/images/vue.jpg" />
+              <CCardImage src="/src/assets/images/vue.jpg" />
             </CCard>
           </CCardGroup>
 
@@ -60,7 +72,7 @@ const DatasetDashboard = () => {
 
 
         <div className="col-5 d-flex flex-column">
-          <h2>Models</h2>
+          <h2>Trained models</h2>
 
           <div style={{ flexGrow: 1 }}>
 
@@ -68,11 +80,12 @@ const DatasetDashboard = () => {
 
               <CAccordion>
                 <CAccordionItem>
-                  <CAccordionHeader>Latent diffusion <br />2024-11-10</CAccordionHeader>
+                  <CAccordionHeader>Latent diffusion 2024-11-10-08-23-35<CBadge className="m-1" color="secondary">30%</CBadge></CAccordionHeader>
                   <CAccordionBody>
                     Parameters: Epochs=30, Learning rate 100
                     <br />
-                    <CButton type="submit" color="primary">Generate image</CButton>
+                    <CButton type="submit" color="primary">Logs</CButton>
+                    <CButton type="submit" color="primary" className="m-2">Stop training</CButton>
                   </CAccordionBody>
                 </CAccordionItem>
                 <CAccordionItem>
@@ -137,7 +150,7 @@ const DatasetDashboard = () => {
 
           <div className="row m-3">
             <div className="col d-grid">
-              <CButton type="submit" color="primary" size='lg' action="#">Train new model</CButton>
+              <CButton color="primary" size='lg' onClick={() => setTrainVisible(true)}>Train new model</CButton>
             </div>
             <div className="col d-grid">
               <CButton type="submit" color="primary" size='lg' action="#">Generate image</CButton>
@@ -145,6 +158,65 @@ const DatasetDashboard = () => {
           </div>
         </div>
       </div>
+
+      <CModal
+        scrollable
+        visible={trainVisible}
+        onClose={() => setTrainVisible(false)}
+        aria-labelledby="TrainModal"
+        size="lg"
+      >
+        <CModalHeader>
+          <CModalTitle id="TrainModal">Train</CModalTitle>
+        </CModalHeader>
+        <CModalBody>
+          <p></p>
+          <CForm>
+
+            <CFormSelect className="mt-2"
+              id="preprocessing"
+              floatingLabel="Preprocessing"
+              options={[
+                { label: 'One', value: '1' },
+                { label: 'Two', value: '2' },
+                { label: 'Three', value: '3' }
+              ]}
+            />
+            <CFormSelect className="mt-2"
+              id="model"
+              floatingLabel="Model"
+              options={[
+                { label: 'Pixel diffusion', value: '1' },
+                { label: 'Latent diffusion', value: '2' },
+              ]}
+            />
+            <CFormInput className="mt-2"
+              id="learning-rate"
+              type="text"
+              floatingLabel="Learning rate"
+              value="1e-10"
+            />
+            <CFormInput className="mt-2"
+              id="max-steps"
+              type="text"
+              floatingLabel="Max steps"
+              value="10000"
+            />
+            <CFormInput className="mt-2"
+              id="filename"
+              type="text"
+              floatingLabel="Filename"
+              value="model-timestamp"
+            />
+
+          </CForm>
+        </CModalBody>
+        <CModalFooter>
+          <CButton color="secondary" onClick={() => setTrainVisible(false)}>Cancel</CButton>
+          <CButton color="primary">Start training</CButton>
+        </CModalFooter>
+      </CModal>
+
     </div>
 
   )
